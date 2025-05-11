22c532520896bfa3be8bb18acd790570f68940e3
Rosemary Kamau <kamaurosemary.n@gmail.com>
2025-05-11 12:38:08 +0300

    Initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def is_anagram(self, other_word):
+        other = other_word.lower()
+        return sorted(self.word) == sorted(other) and self.word != other
+
+    def match(self, words):
+        return [word for word in words if self.is_anagram(word)]
